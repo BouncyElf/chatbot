17f4175cb63f1ece05dac99389873e5631c635c7
liuqiangqiang <qiangqiang@nextbillion.ai>
2025-03-18 15:34:20 +0800

    feat: use another goroutine to handle message

--- handler/handler.go
+++ handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"context"
 	"net/http"
 	"os"
 	"strconv"
@@ -59,10 +60,11 @@ var upgrader = websocket.Upgrader{
 }
 
 type Client struct {
-	conn   *websocket.Conn
-	send   chan []byte
-	client *model.Client
-	state  client_state
+	conn     *websocket.Conn
+	send     chan []byte
+	client   *model.Client
+	msg_recv chan string
+	state    client_state
 }
 
 func handleWS(w http.ResponseWriter, r *http.Request) {
@@ -95,9 +97,10 @@ func handleWS(w http.ResponseWriter, r *http.Request) {
 	}
 
 	c := &Client{
-		conn:   conn,
-		send:   make(chan []byte, 256),
-		client: client,
+		conn:     conn,
+		send:     make(chan []byte, 256),
+		msg_recv: make(chan string, 256),
+		client:   client,
 	}
 
 	go c.writePump()
@@ -126,6 +129,10 @@ func (c *Client) readPump() {
 		}()
 	}
 
+	go func() {
+		c.handleMessage()
+	}()
+
 	for {
 		if c.state == state_over {
 			close(c.send)
@@ -137,63 +144,97 @@ func (c *Client) readPump() {
 			break
 		}
 
-		reply := c.handleMessage(string(message))
-		c.send <- []byte(reply)
-
-		go func() {
-			if c.state == state_handle_feedback {
-				if err := model.SaveFeedback(c.client.ID, string(message)); err != nil {
-					logrus.Error("save feedback error: ", err)
-				} else {
-					logrus.Info("save feedback success: ", string(message))
-				}
-			}
-			if err := model.SaveMessage(c.client.ID, string(reply), true); err != nil {
-				// 使用logrus记录错误日志
-				logrus.Error("save bot message error: ", err)
-			} else {
-				// 使用logrus记录信息日志
-				logrus.Info("save bot message success: ", string(reply))
-			}
-			if err := model.SaveMessage(c.client.ID, string(message), false); err != nil {
-				// 使用logrus记录错误日志
-				logrus.Error("save message error: ", err)
-			} else {
-				// 使用logrus记录信息日志
-				logrus.Info("save message success: ", string(message))
-			}
-		}()
+		c.msg_recv <- string(message)
 	}
 }
 
-func (c *Client) handleMessage(message string) string {
-	// 使用logrus记录信息日志
-	logrus.Info(message)
-	for _, v := range BotMsgTemp.ReviewKeywords {
-		if strings.Contains(message, v) {
-			c.state = state_handle_feedback
-			return BotMsgTemp.StartReview
+func (c *Client) saveMessageAndReply(message, reply string) {
+	if c.state == state_handle_feedback {
+		if err := model.SaveFeedback(c.client.ID, message); err != nil {
+			logrus.Error("save feedback error: ", err)
+		} else {
+			logrus.Info("save feedback success: ", message)
 		}
 	}
-	for _, v := range BotMsgTemp.EndKeywords {
-		if strings.Contains(message, v) {
-			c.state = state_over
-			return BotMsgTemp.Ending
+	if err := model.SaveMessage(c.client.ID, reply, true); err != nil {
+		// 使用logrus记录错误日志
+		logrus.Error("save bot message error: ", err)
+	} else {
+		// 使用logrus记录信息日志
+		logrus.Info("save bot message success: ", reply)
+	}
+	if err := model.SaveMessage(c.client.ID, message, false); err != nil {
+		// 使用logrus记录错误日志
+		logrus.Error("save message error: ", err)
+	} else {
+		// 使用logrus记录信息日志
+		logrus.Info("save message success: ", message)
+	}
+}
+
+func (c *Client) innerHandle(ctx context.Context, messages []string, reply_chan chan string) {
+	if len(messages) == 0 || messages[0] == "" {
+		return
+	}
+	logrus.Info(messages)
+	message := messages[0]
+	reply := BotMsgTemp.Default
+	select {
+	case <-ctx.Done():
+		return
+	case <-time.After(10 * time.Second):
+		// 使用logrus记录信息日志
+		for _, v := range BotMsgTemp.ReviewKeywords {
+			if strings.Contains(message, v) {
+				c.state = state_handle_feedback
+				reply = BotMsgTemp.StartReview
+				break
+			}
+		}
+		for _, v := range BotMsgTemp.EndKeywords {
+			if strings.Contains(message, v) {
+				c.state = state_over
+				reply = BotMsgTemp.Ending
+				break
+			}
+		}
+		for k, v := range BotMsgTemp.Keywords {
+			if strings.Contains(message, k) {
+				// 使用logrus记录信息日志
+				logrus.Info(message, k, v, strings.Contains(message, k))
+				reply = v
+				break
+			}
+		}
+		if c.state == state_wait_feedback {
+			reply = BotMsgTemp.GuideToFeedback
+		} else if c.state == state_handle_feedback {
+			reply = BotMsgTemp.StartReview
+		}
+		reply_chan <- reply
+	}
+}
+
+func (c *Client) handleMessage() {
+	last_msg := <-c.msg_recv
+	last_msgs := []string{last_msg}
+	for {
+		ctx := context.Background()
+		subCtx, cancelF := context.WithCancel(ctx)
+		reply_chan := make(chan string)
+		go c.innerHandle(subCtx, last_msgs, reply_chan)
+		select {
+		case new_msg := <-c.msg_recv:
+			cancelF()
+			last_msgs = append([]string{new_msg}, last_msgs...)
+		case reply, ok := <-reply_chan:
+			if ok {
+				c.send <- []byte(reply)
+				go c.saveMessageAndReply(last_msg, reply)
+				last_msgs = []string{}
+			}
 		}
 	}
-	for k, v := range BotMsgTemp.Keywords {
-		if strings.Contains(message, k) {
-			// 使用logrus记录信息日志
-			logrus.Info(message, k, v, strings.Contains(message, k))
-			return v
-		}
-	}
-	if c.state == state_wait_feedback {
-		return BotMsgTemp.GuideToFeedback
-	} else if c.state == state_handle_feedback {
-		return BotMsgTemp.StartReview
-	}
-	return BotMsgTemp.Default
 }
 
 func (c *Client) writePump() {
